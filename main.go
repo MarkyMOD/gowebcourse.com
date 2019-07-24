0353eff306090c78044be2a809e9f2d2b1c9ddd3
Not John Tulsi <strictlybusinessemail242@gmail.com>
2019-07-24 14:25:49 -0600

    added a fresh config file

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello Wor</h1>")
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Yo</h1>")
+}
 
 func main() {
-	fmt.Println("HI")
+	http.HandleFunc("/", index)
+	http.HandleFunc("/about", about)
+	fmt.Println("Server Starting...")
+	http.ListenAndServe(":3000", nil)
 }
